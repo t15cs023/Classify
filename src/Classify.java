e3fe3b10dac9269338a7cbefc9572a75e308bad0
t15cs023 <t15cs023@bw10.kki.yamanashi.ac.jp>
2018-01-11 18:49:36 +0900

    [add]queryのtfidfを追加

--- src/Classify.java
+++ src/Classify.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 public class Classify {
 
+	public static double N = 8.0;
+	
 	/**
 	 * 主な処理
 	 * @param args
@@ -12,7 +14,7 @@ public class Classify {
 	public static void main(String[] args) {
 		ArrayList<ArrayList<Word>> list = tfidfrun();
 		//ArrayList<ArrayList<Word>> list = freqrun();
-		ArrayList<ArrayList<Word>> queryList = queryrun();
+		ArrayList<ArrayList<Word>> queryList = queryrun(list);
 		ArrayList<ArrayList<Result>> searchResultCos = new ArrayList<>();
 		ArrayList<ArrayList<Result>> searchResultCosWithoutNos = new ArrayList<>();
 		ArrayList<ArrayList<Result>> searchResultJac = new ArrayList<>();
@@ -27,6 +29,7 @@ public class Classify {
 			for(int j = 0; j < list.size(); j++) {
 				listResult[j] = cosineCalc(list.get(j),queryList.get(i));
 			}
+			System.out.println();
 			ArrayList<Result> result = findLargests(listResult, list.size());
 			searchResultCos.add(result);
 		}
@@ -166,32 +169,6 @@ public class Classify {
 		System.out.println(result);
 		return result;
 	}
-
-	private static double cosineCalcWithoutTFIDF(ArrayList<Word> subWordList, ArrayList<Word> subQueryList) {
-		double x2 = 0.0;
-		for(int i = 0; i < subWordList.size(); i++) {
-			x2 += 1;
-		}
-		double y2 = 0.0;
-		for(int i = 0; i < subQueryList.size(); i++) {
-			y2 += 1;
-		}
-		double inner_product = 0.0;
-		for(int i = 0; i < subQueryList.size(); i++) {
-			String currentQSubWord = subQueryList.get(i).return_word();
-			for(int j = 0; j < subWordList.size(); j++) {
-				String currentLSubWord = subWordList.get(j).return_word();
-				if(currentQSubWord.equals(currentLSubWord)) {
-					inner_product += 1; 
-				}
-			}
-		}
-		double down = Math.sqrt(x2*y2);
-		double result = inner_product/down;
-		System.out.println("x2:" + x2 + " y2:" + y2 + " inner_product:" + inner_product);
-		System.out.println(result);
-		return result;
-	}
 	
 	private static double cosineCalcWithoutNos(ArrayList<Word> subWordList, ArrayList<Word> subQueryList) {
 		double x2 = 0.0;
@@ -288,29 +265,6 @@ public class Classify {
 					}
 					if(!flag)
 						inner_product += currentQSubVal * currentLSubVal; 
-				}
-			}
-		}
-		double up = inner_product;
-		double down = x2+y2-inner_product;
-		double result = up/down;
-		System.out.println("x2:" + x2 + " y2:" + y2 + " inner_product:" + inner_product);
-		System.out.println(result);
-		return result;
-	}
-	
-	private static double jaccardCalcWithoutTFIDF(ArrayList<Word> subWordList, ArrayList<Word> subQueryList) {
-		double x2 = subWordList.size();
-		double y2 = subQueryList.size();
-		double inner_product = 0.0;
-		for(int i = 0; i < subQueryList.size(); i++) {
-			String currentQSubWord = subQueryList.get(i).return_word();
-			double currentQSubVal = subQueryList.get(i).return_val();
-			for(int j = 0; j < subWordList.size(); j++) {
-				String currentLSubWord = subWordList.get(j).return_word();
-				double currentLSubVal = subWordList.get(j).return_val();
-				if(currentQSubWord.equals(currentLSubWord)) {
-					inner_product += currentQSubVal * currentLSubVal; 
 				}
 			}
 		}
@@ -456,18 +410,16 @@ public class Classify {
 			for(int x = 0; x < train_width+1; x++) {
 				trainwtfidf[y][x] = trainwdf[y][x];
 			}
-			trainwtfidf[y][train_width+1] = Double.toString(Double.parseDouble(trainwtfidf[y][2]) * (log2(10.0/Double.parseDouble(trainwtfidf[y][3])) +1));
+			trainwtfidf[y][train_width+1] = Double.toString(Double.parseDouble(trainwtfidf[y][2]) * (log2(N/Double.parseDouble(trainwtfidf[y][3])) +1));
 		}
 
 		/* 最終的なリスト確認用 */
-		/*
 		for(int y = 0; y < train_height; y++) {
 			for(int x = 0; x < 5; x++) {
 				System.out.print(trainwtfidf[y][x] + " ");
 			}
 			System.out.println();
 		}
-		 */
 
 		/* 管理を簡単にするために、ArrayListに保存する */
 		ArrayList<ArrayList<Word>> l = new ArrayList<>();
@@ -478,13 +430,14 @@ public class Classify {
 			String genre[] = {"国際", "経済", "家庭", "科学", "芸能", "スポーツ", "文化", "読書"};
 			for(int y = 0; y < train_height; y++) {
 				if(trainwtfidf[y][0].equals(genre[i])) {
-					w.add(new Word(trainwtfidf[y][1], Double.parseDouble(trainwtfidf[y][4])));
+					w.add(new Word(trainwtfidf[y][1], Double.parseDouble(trainwtfidf[y][4]), Double.parseDouble(trainwtfidf[y][3])));
 				}
 			}
 			l.add(w);
 		}
 
 		/* リスト確認用 */
+		/*
 		for(int i = 0; i < l.size(); i++) {
 			String genre[] = {"国際", "経済", "家庭", "科学", "芸能", "スポーツ", "文化", "読書"};
 			System.out.println();
@@ -494,10 +447,14 @@ public class Classify {
 				System.out.println(currentw.return_word() + " " + currentw.return_val());
 			}
 		}
-
+		 */
 		return l;
 	}
 	
+	/**
+	 * 訓練データの各ジャンルの単語にTFを重みとする。
+	 * @return ジャンル別に単語とそのTFからなるWord型のリストをまとめるリスト
+	 */
 	public static ArrayList<ArrayList<Word>> freqrun() {
 		/* ここからtrain.freqのファイル読み込み処理 */
 		ArrayList<ArrayList<String>> temp = new ArrayList<>();
@@ -601,12 +558,14 @@ public class Classify {
 		}
 
 		/* 最終的なリスト確認用 */
+		/*
 		for(int y = 0; y < train_height; y++) {
 			for(int x = 0; x < train_width+3; x++) {
 				System.out.print(trainwnw[y][x] + " ");
 			}
 			System.out.println();
 		}
+		*/
 		
 		/* 管理を簡単にするために、ArrayListに保存する */
 		ArrayList<ArrayList<Word>> l = new ArrayList<>();
@@ -617,7 +576,7 @@ public class Classify {
 			String genre[] = {"国際", "経済", "家庭", "科学", "芸能", "スポーツ", "文化", "読書"};
 			for(int y = 0; y < train_height; y++) {
 				if(trainwnw[y][0].equals(genre[i])) {
-					w.add(new Word(trainwnw[y][1], Double.parseDouble(trainwnw[y][2])));
+					w.add(new Word(trainwnw[y][1], Double.parseDouble(trainwnw[y][2]), 0));
 				}
 			}
 			l.add(w);
@@ -633,13 +592,14 @@ public class Classify {
 		*/
 		return l;
 	}
+	
 	/**
 	 * テストデータの読み込みを行い、
 	 * 1つの文書に属する単語とその出現回数を1つのリストに保存し、全文書を扱うリストにそのリストを保存する。
 	 * 返り値が全文書のリストを扱うリストである。
 	 * @return 全文書のリストを扱うリスト
 	 */
-	public static ArrayList<ArrayList<Word>> queryrun() {
+	public static ArrayList<ArrayList<Word>> queryrun(ArrayList<ArrayList<Word>> train_list) {
 		ArrayList<ArrayList<Word>> list = new ArrayList<>();
 		/* ここからquery.freqのファイル読み込み処理 */
 		ArrayList<ArrayList<String>> query = new ArrayList<>();
@@ -666,27 +626,76 @@ public class Classify {
 		catch(IOException e) {
 			;
 		}
-
+		String query_[][];
+		int query_height, query_width;
+		query_height = query.size();
+		query_width = query.get(0).size();
+		query_ = new String[query_height][query_width];
+		for(int y = 0; y < query_height; y++) {
+			for(int x = 0; x < query_width; x++) {
+				query_[y][x] = query.get(y).get(x);
+			}
+		}
+		
+		String querywdf[][] = new String[query_height][query_width+1];
+		for(int y = 0; y < query_height; y++) {
+			for(int x = 0; x < query_width; x++) {
+				querywdf[y][x] = query_[y][x];
+			}
+			querywdf[y][query_width] = "0";
+		}
+		
+		for(int y = 0; y < query_height; y++) {
+			for(int ysub = 0; ysub < query_height; ysub++) {
+				if(querywdf[y][1].equals(querywdf[ysub][1])) {
+					int count = Integer.parseInt(querywdf[y][query_width]);
+					count++;
+					querywdf[y][query_width] = Integer.toString(count);
+				}
+			}
+		}
+		
+		String querywtfidf[][] = new String[query_height][query_width+2];
+		for(int y = 0; y < query_height; y++) {
+			for(int x = 0; x < query_width+1; x++) {
+				querywtfidf[y][x] = querywdf[y][x];
+			}
+			boolean flag = false;
+			querywtfidf[y][query_width+1] = "0.0";
+			for(int i = 0; i < train_list.size(); i++) {
+				for(int j = 0; j < train_list.get(i).size(); j++) {
+					if(querywtfidf[y][1].equals(train_list.get(i).get(j).return_word())) {
+						flag = true;
+						querywtfidf[y][query_width+1] = Double.toString(Double.parseDouble(querywtfidf[y][2]) * (log2(N/train_list.get(i).get(j).return_df_val()) + 1));
+					}
+				}
+				if(flag)
+					break;
+			}
+		}
+		
 		/* 確認用 */
 		/*
-		for(int i = 0; i < query.size(); i++) {
-			for(int j = 0; j < query.get(i).size(); j++) {
-				System.out.print(query.get(i).get(j) + " ");
+		for(int y = 0; y < query_height; y++) {
+			for(int x = 0; x < query_width+2; x++) {
+				System.out.print(querywtfidf[y][x] + " ");
 			}
 			System.out.println();
 		}
-		 */
+		*/
 		/* ここまでquery.freqのファイル読み込み処理 */
+		
 		for(int i = 1; i <= 10; i++) {
 			ArrayList<Word> w = new ArrayList<>();
 			for(int j = 0; j < query.size(); j++) {
 				if(query.get(j).get(0).equals(Integer.toString(i))) {
-					Word currentWord = new Word(query.get(j).get(1), Double.parseDouble(query.get(j).get(2)));
+					Word currentWord = new Word(querywtfidf[j][1], Double.parseDouble(querywtfidf[j][query_width+1]), 0);
 					w.add(currentWord);
 				}
 			}
 			list.add(w);
 		}
+		
 		for(int i= 0; i < list.size(); i++) {
 			for(int j = 0; j < list.get(i).size(); j++) {
 				Word w = list.get(i).get(j);
--- src/Word.java
+++ src/Word.java
@@ -1,6 +1,7 @@
 public class Word {
 	/* tf*idf重み */
 	private double tf_idf_value;
+	private double df_value;
 	/* 単語 */
 	private String word;
 	
@@ -9,8 +10,9 @@ public class Word {
 	 * @param w 単語
 	 * @param val tf*idf重み
 	 */
-	public Word(String w, double val) {
+	public Word(String w, double val, double df_val) {
 		this.tf_idf_value = val;
+		this.df_value = df_val;
 		this.word = w;
 	}
 	
@@ -20,6 +22,14 @@ public class Word {
 	 */
 	public double return_val() {
 		return tf_idf_value;
+	}
+	
+	/**
+	 * 重みを返す
+	 * @return df重み
+	 */
+	public double return_df_val() {
+		return df_value;
 	}
 	
 	/**
